dc8cab0a479325362d2101e4ba49965bc7140493
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2026-08-30 23:35:40 +0200

    Portapps 3.19.0

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/portapps/portapps/v3"
 	"github.com/portapps/portapps/v3/pkg/log"
-	"github.com/portapps/portapps/v3/pkg/utl"
 )
 
 var (
@@ -25,32 +24,36 @@ func init() {
 }
 
 func main() {
-	utl.CreateFolder(app.DataPath)
+	if err := os.MkdirAll(app.DataPath, os.ModePerm); err != nil {
+		log.Fatal().Err(err).Msg("Cannot create data directory.")
+	}
 	app.Process = filepath.Join(app.AppPath, "bin", "smartgit.exe")
 	app.WorkingDir = filepath.Join(app.AppPath, "bin")
 
 	// create err folder
-	utl.CreateFolder(app.DataPath, "err")
+	if err := os.MkdirAll(filepath.Join(app.DataPath, "err"), os.ModePerm); err != nil {
+		log.Fatal().Err(err).Msg("Cannot create error directory.")
+	}
 
 	// create default smartgit.vmoptions if not found
 	customSmartgitOptionsPath := filepath.Join(app.DataPath, "smartgit.vmoptions")
-	if !utl.Exists(customSmartgitOptionsPath) {
-		if err := utl.CreateFile(customSmartgitOptionsPath, `-Xmx1024m
+	if _, err := os.Stat(customSmartgitOptionsPath); os.IsNotExist(err) {
+		if err := os.WriteFile(customSmartgitOptionsPath, []byte(`-Xmx1024m
 -Dsmartgit.disableBugReporting=true
-`); err != nil {
+`), 0644); err != nil {
 			log.Fatal().Err(err).Msg("Cannot write default smartgit.vmoptions")
 		}
 	}
 
 	// override system smartgit.vmoptions
 	smartgitOptionsPath := filepath.Join(app.AppPath, "bin", "smartgit.vmoptions")
-	if err := utl.CreateFile(smartgitOptionsPath, strings.Replace(`-Dsmartboot.sourceDirectory={{ DATA_PATH }}\.updates
+	if err := os.WriteFile(smartgitOptionsPath, []byte(strings.Replace(`-Dsmartboot.sourceDirectory={{ DATA_PATH }}\.updates
 -Dsmartgit.settings={{ DATA_PATH }}\.settings
 -Dsmartgit.updateCheck.enabled=false
 -Dsmartgit.updateCheck.automatic=false
 -XX:ErrorFile={{ DATA_PATH }}\err\hs_err_pid%p.log
 -include-options {{ DATA_PATH }}\smartgit.vmoptions
-`, "{{ DATA_PATH }}", utl.FormatWindowsPath(app.DataPath), -1)); err != nil {
+`, "{{ DATA_PATH }}", filepath.FromSlash(app.DataPath), -1)), 0644); err != nil {
 		log.Fatal().Err(err).Msg("Cannot write system smartgit.vmoptions")
 	}
 
